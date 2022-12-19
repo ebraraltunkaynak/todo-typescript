c2f4f409e9a2b7779aa13eb355e0ce87844ad409
Ebrar <16155052@mersin.edu.tr>
2022-12-20 01:41:13 +0300

    defined the type

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{FC,ChangeEvent,useState} from 'react'
+import './App.css'
+import {ITask} from './Interfaces'
 
-function App() {
+
+const App: FC = () => {
+  const [task,setTask] = useState<string>("")
+ const [deadline,setDeadline] = useState<number>(0)
+  const [todoList,setTodoList] = useState<ITask[]>([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void =>{
+
+    if(event.target.name ===  "task" )  {
+      setTask(event.target.value)
+    }else {
+      setDeadline(Number(event.target.value))
+    }
+
+    setTask(event.target.value);
+  };
+
+ const addTask = () : void => {
+  const newTask = {taskName:task,deadline:deadline}
+setTodoList([...todoList,newTask])
+
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+ 
+     <div className="header">
+
+      <div className="inputContainer">
+
+   
+      <input type="text" placeholder="Enter Task" name='task'  onChange={handleChange}/>
+      <input type="number" min="0" placeholder="Enter Time"  name='deadline' onChange={handleChange}/>
+      </div>
+      <button onClick={addTask}>Add Task</button>
+
+     </div>
+     <div className="todolist"></div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
