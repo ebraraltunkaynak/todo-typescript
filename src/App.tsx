5e4227d9cad0a7ca2e1551ad34af112760dfb2f2
Ebrar <16155052@mersin.edu.tr>
2022-12-20 13:01:04 +0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,58 @@
-import React,{FC,ChangeEvent,useState} from 'react'
+import React, { FC, ChangeEvent, useState } from 'react'
 import './App.css'
-import {ITask} from './Interfaces'
+import Todo from './components/Todo'
+import { ITask } from './Interfaces'
 
 
 const App: FC = () => {
-  const [task,setTask] = useState<string>("")
- const [deadline,setDeadline] = useState<number>(0)
-  const [todoList,setTodoList] = useState<ITask[]>([])
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todoList, setTodoList] = useState<ITask[]>([])
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void =>{
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
 
-    if(event.target.name ===  "task" )  {
+    if (event.target.name === "task") {
       setTask(event.target.value)
-    }else {
+    } else {
       setDeadline(Number(event.target.value))
     }
 
-    setTask(event.target.value);
+    // setTask(event.target.value);
   };
 
- const addTask = () : void => {
-  const newTask = {taskName:task,deadline:deadline}
-setTodoList([...todoList,newTask])
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline }
+    setTodoList([...todoList, newTask])
+    setTask("")
+    setDeadline(0)
 
- }
+  }
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter(task =>{ 
+
+      return task.taskName !== taskNameToDelete
+    }));
+  }
+
   return (
     <div className='App'>
- 
-     <div className="header">
 
-      <div className="inputContainer">
+      <div className="header">
 
-   
-      <input type="text" placeholder="Enter Task" name='task'  onChange={handleChange}/>
-      <input type="number" min="0" placeholder="Enter Time"  name='deadline' onChange={handleChange}/>
+        <div className="inputContainer">
+
+
+          <input  type="text" placeholder="Enter Task" name='task' value={task} onChange={handleChange}   />
+          <input type="number" min="1" placeholder="Enter Time" name='deadline' value={deadline} onChange={handleChange} />
+        </div>
+        <button type='submit' onClick={addTask}>Add Task</button>
+
       </div>
-      <button onClick={addTask}>Add Task</button>
-
-     </div>
-     <div className="todolist"></div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <Todo key={key} task={task} completeTask={completeTask} />
+        })}
+      </div>
 
     </div>
   )
